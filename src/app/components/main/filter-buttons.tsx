9b8fb9ec7d20d053871e951cda5996247deddc90
gguilhermelopes <guilhermelopes275@gmail.com>
2023-06-26 02:49:26 -0300

    fix: filters

--- src/app/components/main/filter-buttons.tsx
+++ src/app/components/main/filter-buttons.tsx
@@ -2,20 +2,32 @@
 import useTasks from "@/hooks/useTasks";
 import { FilterButton } from "./styles";
 import { CircularProgress } from "@mui/joy";
+import { useContext } from "react";
+import { TasksContext } from "@/context/tasksContext";
 
 const FilterButtons = () => {
   const { fetchCompletedTasks, fetchTasks, fetchActiveTasks, loading } =
     useTasks();
+  const { tasks, setTasks } = useContext(TasksContext);
 
   const handleAllClick = async () => await fetchTasks();
-  const handleActiveClick = async () => await fetchActiveTasks();
+  const handleFilterClick = async (isActive: boolean) => {
+    await fetchTasks();
+    setTasks((prevState) =>
+      prevState.filter((task) => (isActive ? !task.isChecked : task.isChecked))
+    );
+  };
   const handleCompletedClick = async () => await fetchCompletedTasks();
 
   return (
     <>
       <FilterButton onClick={handleAllClick}>All</FilterButton>
-      <FilterButton onClick={handleActiveClick}>Active</FilterButton>
-      <FilterButton onClick={handleCompletedClick}>Completed</FilterButton>
+      <FilterButton onClick={() => handleFilterClick(true)}>
+        Active
+      </FilterButton>
+      <FilterButton onClick={() => handleFilterClick(false)}>
+        Completed
+      </FilterButton>
       {loading && <CircularProgress color="info" size="sm" />}
     </>
   );
